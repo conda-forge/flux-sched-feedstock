07dc4d9ee28b0d66480bda2d520a9f98631fced8
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-01-30 11:36:32 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export LDFLAGS="${LDFLAGS} -lboost_system -lboost_graph -lboost_filesystem -lboost_regex"
 export CXXFLAGS="${CXXFLAGS} -std=c++14 -Wno-maybe-uninitialized -I${PREFIX}/include -I${PREFIX}/include/boost"
-./configure --prefix=${PREFIX} 
+cmake --prefix=${PREFIX} ${CMAKE_ARGS} .
 
 make V=1
 
