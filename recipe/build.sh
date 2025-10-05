3600c169900f83e608b0b080d4a054f7cf907798
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-10-05 22:42:51 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 export LDFLAGS="${LDFLAGS} -lboost_system -lboost_graph -lboost_filesystem -lboost_regex"
 export CXXFLAGS="${CXXFLAGS} -std=c++14 -Wno-maybe-uninitialized -I${PREFIX}/include -I${PREFIX}/include/boost"
-cmake -B build --preset default ${CMAKE_ARGS}
+cmake --build build --preset default ${CMAKE_ARGS}
 cmake --build build
 # ctest --test-dir build
-cmake --build build -t install
-
-# Manual installation
-mkdir -p ${PREFIX}/lib
-mkdir -p ${PREFIX}/lib/modules
-cp ${SRC_DIR}/build/lib/libfluxion-data.so.0.47.0 ${PREFIX}/lib
-cp ${SRC_DIR}/build/lib/libfluxion-data.so.0.47 ${PREFIX}/lib
-cp ${SRC_DIR}/build/lib/libsched-fluxion-resource-module.so ${PREFIX}/lib
-cp ${SRC_DIR}/build/lib/modules/sched-fluxion-resource.so ${PREFIX}/lib/modules
-cp ${SRC_DIR}/build/lib/modules/sched-fluxion-feasibility.so ${PREFIX}/lib/modules
-cp ${SRC_DIR}/build/lib/modules/sched-fluxion-qmanager.so ${PREFIX}/lib/modules
-cp ${SRC_DIR}/build/lib/libreapi_cli.so ${PREFIX}/lib
+cmake --build build --target install
