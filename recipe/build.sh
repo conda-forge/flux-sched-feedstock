1efd97a0672d47d25261cbcd25339f83911909df
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-10-05 19:56:02 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 export LDFLAGS="${LDFLAGS} -lboost_system -lboost_graph -lboost_filesystem -lboost_regex"
 export CXXFLAGS="${CXXFLAGS} -std=c++14 -Wno-maybe-uninitialized -I${PREFIX}/include -I${PREFIX}/include/boost"
-cmake ${CMAKE_ARGS} .
 
-make V=1
-
-export FLUX_TESTS_LOGFILE=t
+mkdir build
+cd build
+../configure --prefix=${FLUX_CORE_PREFIX}
+make
 make check
-
 make install
-
-ls $PREFIX/lib
