e1745665743c242edc09d4e7dadd75f7f6307103
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-10-05 19:48:07 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export LDFLAGS="${LDFLAGS} -lboost_system -lboost_graph -lboost_filesystem -lboost_regex"
 export CXXFLAGS="${CXXFLAGS} -std=c++14 -Wno-maybe-uninitialized -I${PREFIX}/include -I${PREFIX}/include/boost"
-cmake -B build --preset default ${CMAKE_ARGS}
+cmake -B build --preset default ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX="${PREFIX}"
 cmake --build build
 ctest --test-dir build
 cmake --build build -t install
