e15bf0cac883f07b4f2cead2a44eb8e3a5094692
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-10-05 17:38:31 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 export LDFLAGS="${LDFLAGS} -lboost_system -lboost_graph -lboost_filesystem -lboost_regex"
 export CXXFLAGS="${CXXFLAGS} -std=c++14 -Wno-maybe-uninitialized -I${PREFIX}/include -I${PREFIX}/include/boost"
-cmake ${CMAKE_ARGS} .
-
-make V=1
-
-export FLUX_TESTS_LOGFILE=t
-make check
-
-make install
+cmake -B build --preset default ${CMAKE_ARGS}
+cmake --build build
+ctest --test-dir build
+cmake --build build -t install
+ls $PREFIX/lib
