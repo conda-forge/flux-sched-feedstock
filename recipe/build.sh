706667bd25b2eb25528544f6939a1e424f6186b1
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-10-06 09:14:44 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,12 +7,15 @@ cmake --build build
 cmake --build build --target install
 
 # Manual installation
+ls ${PREFIX}/lib
+ls ${PREFIX}/lib/flux
+ls ${PREFIX}/lib/flux/modules
 mkdir -p ${PREFIX}/lib
 mkdir -p ${PREFIX}/lib/modules
 cp ./lib/libfluxion-data.so.0.47.0 ${PREFIX}/lib
 cp ./lib/libfluxion-data.so.0.47 ${PREFIX}/lib
 cp ./lib/libsched-fluxion-resource-module.so ${PREFIX}/lib
-cp ./lib/modules/sched-fluxion-resource.so ${PREFIX}/lib/modules
-cp ./lib/modules/sched-fluxion-feasibility.so ${PREFIX}/lib/modules
-cp ./lib/modules/sched-fluxion-qmanager.so ${PREFIX}/lib/modules
-cp ./lib/libreapi_cli.so ${PREFIX}/lib
+cp ./build/resource/modules/sched-fluxion-resource.so ${PREFIX}/lib/modules
+cp ./build/resource/modules/sched-fluxion-feasibility.so ${PREFIX}/lib/modules
+cp ./build/resource/modules/sched-fluxion-qmanager.so ${PREFIX}/lib/modules
+cp ./build/resource/reapi/bindings/libreapi_cli.so ${PREFIX}/lib
