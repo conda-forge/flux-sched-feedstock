c6a24aa60c4aad2371f5bfaa561f2470cb39024d
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-10-06 09:32:58 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,18 +4,9 @@ export CXXFLAGS="${CXXFLAGS} -std=c++14 -Wno-maybe-uninitialized -I${PREFIX}/inc
 cmake -B build --preset default ${CMAKE_ARGS}
 cmake --build build
 # ctest --test-dir build
-cmake --build build --target install
+cmake --build build --target install --debug-output
 
 # Manual installation
-ls ${PREFIX}/lib
-ls ${PREFIX}/lib/flux
-ls ${PREFIX}/lib/flux/modules
-mkdir -p ${PREFIX}/lib
-mkdir -p ${PREFIX}/lib/modules
 cp ./lib/libfluxion-data.so.0.47.0 ${PREFIX}/lib
 cp ./lib/libfluxion-data.so.0.47 ${PREFIX}/lib
 cp ./lib/libsched-fluxion-resource-module.so ${PREFIX}/lib
-cp ./build/resource/modules/sched-fluxion-resource.so ${PREFIX}/lib/modules
-cp ./build/resource/modules/sched-fluxion-feasibility.so ${PREFIX}/lib/modules
-cp ./build/resource/modules/sched-fluxion-qmanager.so ${PREFIX}/lib/modules
-cp ./build/resource/reapi/bindings/libreapi_cli.so ${PREFIX}/lib
