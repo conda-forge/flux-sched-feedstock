8523b8cd3368b5cefaea655d3ccad967a97aa4da
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-10-06 12:34:51 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 export LDFLAGS="${LDFLAGS} -lboost_system -lboost_graph -lboost_filesystem -lboost_regex"
 export CXXFLAGS="${CXXFLAGS} -std=c++14 -Wno-maybe-uninitialized -I${PREFIX}/include -I${PREFIX}/include/boost"
+
+# cmake build
 cmake -B build --preset default ${CMAKE_ARGS}
 cmake --build build
-ctest --test-dir build
+
+# Do not test on Arch
+if [[ -z "${CONDA_BUILD_CROSS_COMPILATION}" ]]; then
+  ctest --test-dir build
+fi
+
+# cmake installation
 cmake --build build --target install
 
-# Manual installation
+# manual installation
 cp ./lib/* ${PREFIX}/lib
